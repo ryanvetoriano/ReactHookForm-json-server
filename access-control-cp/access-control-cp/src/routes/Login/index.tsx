c3b658ca5d8e1f1208f637f0b3196a15f4d2d6f0
Raul_Rezende <rm564002@fiap.com.br>
2025-10-03 20:59:30 -0300

    feat - adicionando formulários tipados com react-hook-form incluindo validação e mensagens de erro para a rota login

--- access-control-cp/access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Login/index.tsx
@@ -1,9 +1,12 @@
+import { useForm } from "react-hook-form";
+import type { TipoLogin } from "../../types/tipoLogin";
 
-export default function Login(){
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TipoLogin>();
 
-    return(
-        <main>
-            
-        </main>
-    )
+  return <main></main>;
 }

b7bb78c0211ba2283f4af2ac79f7fc300b01290f
Raul_Rezende <rm564002@fiap.com.br>
2025-10-03 20:30:00 -0300

    feat - adicionando verificação de duplicidade de nomeUsuario e email no cadastro

--- access-control-cp/access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Cadastro/index.tsx
@@ -8,7 +8,17 @@ export default function Cadastro() {
     formState: { errors },
   } = useForm<User>();
 
-  const onSubmit = async (data: User) => {};
+  const onSubmit = async (data: User) => {
+    const res = await fetch(
+      `http://localhost:3001/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`
+    );
+    const users: User[] = await res.json();
+
+    if (users.length > 0) {
+      alert("Usuário ou email já cadastrados!");
+      return;
+    }
+  };
 
   return <main></main>;
 }

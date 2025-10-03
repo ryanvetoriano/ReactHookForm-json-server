a72c645777b6c81186e20d740e8876d463f0c478
Ryan Vetoriano <vetorianosilva@gmail.com>
2025-10-03 20:17:55 -0300

    feat - adicionando formulários tipados com react-hook-form incluindo validação e mensagens de erro

--- access-control-cp/access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,5 +1,13 @@
+import { useForm } from "react-hook-form";
+import type { User } from "../../types/tipoUsuario";
 
 export default function Cadastro(){
+
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>();
 
     return(
         <main>

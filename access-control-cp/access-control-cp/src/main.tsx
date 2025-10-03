b9c3b1838251d96899e882366af56324b79793e7
Ryan Vetoriano <vetorianosilva@gmail.com>
2025-10-03 19:55:27 -0300

    feat - Adicionando RouteProvider no main e os endereços das rotas

--- access-control-cp/access-control-cp/src/main.tsx
+++ access-control-cp/access-control-cp/src/main.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './routes/Error/index.tsx';
+import Login from './routes/Login/index.tsx';
+import Cadastro from './routes/Cadastro/index.tsx';
+import Home from './routes/Home/index.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error/>,
+    children: [
+      { path: "/", element: <Login/> },
+      { path: "/cadastro", element: <Cadastro/> },
+      { path: "/home", element: <Home/> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);

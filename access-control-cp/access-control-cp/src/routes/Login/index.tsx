e1b52424e82abcb8de994572c9257bae2673c14b
Raul_Rezende <rm564002@fiap.com.br>
2025-10-03 21:05:18 -0300

    feat - implementa lógica de login com validação no JSON Server e armazenamento no localStorage

--- access-control-cp/access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Login/index.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import type { TipoLogin } from "../../types/tipoLogin";
 import type { User } from "../../types/tipoUsuario";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const {
@@ -9,7 +10,22 @@ export default function Login() {
     formState: { errors },
   } = useForm<TipoLogin>();
 
-  const onSubmit = async (data: User) => {};
-  
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: User) => {
+    const res = await fetch(
+      `http://localhost:3001/usuarios?nomeUsuario=${data.nomeUsuario}&email=${data.email}`
+    );
+    const users: User[] = await res.json();
+
+    if (users.length === 0) {
+      alert("Usuário ou email inválido!");
+      return;
+    }
+
+    localStorage.setItem("loggedUser", JSON.stringify(users[0]));
+    navigate("/home");
+  };
+
   return <main></main>;
 }

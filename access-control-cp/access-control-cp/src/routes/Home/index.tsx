33afbea48d3d0863cc2cdcd551263359fabe4a86
PietroDonella <pietrosalomao1@gmail.com>
2025-10-03 21:28:36 -0300

    feat - Capturando as informações do usuario logado e apresentando elas na home

--- access-control-cp/access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Home/index.tsx
@@ -1,8 +1,19 @@
-
-
 export default function Home() {
   const user = localStorage.getItem("loggedUser");
   const parsedUser = user ? JSON.parse(user) : null;
 
-  return <main></main>;
+  return (
+    <main>
+      <h1>Home</h1>
+      <div>
+        {parsedUser ? (
+          <p>
+            Bem-vindo, <b>{parsedUser.nome}</b> ({parsedUser.email})
+          </p>
+        ) : (
+          <p>Você não está logado.</p>
+        )}
+      </div>
+    </main>
+  );
 }

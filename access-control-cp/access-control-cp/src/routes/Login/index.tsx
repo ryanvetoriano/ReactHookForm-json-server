98b239485edbf56f3ef49378490ae338a9b742a2
Raul_Rezende <rm564002@fiap.com.br>
2025-10-03 21:03:32 -0300

    feat - criação da função onSubmit para captura de dados do formulário no login

--- access-control-cp/access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/access-control-cp/src/routes/Login/index.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import type { TipoLogin } from "../../types/tipoLogin";
+import type { User } from "../../types/tipoUsuario";
 
 export default function Login() {
   const {
@@ -8,5 +9,7 @@ export default function Login() {
     formState: { errors },
   } = useForm<TipoLogin>();
 
+  const onSubmit = async (data: User) => {};
+  
   return <main></main>;
 }
